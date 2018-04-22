eca12c05546f1f534a35c4c56be1e6cba73b80e0
Paolo Speziali <speziali99@gmail.com>
2018-04-22 15:34:06 +0200

    Primo commit

---
+++ Filter.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Filter
+ *
+ * @author paolo
+ */
+class Filter {
+    //put your code here
+    
+    public function sanitize($data) {
+        $data = strip_tags($data);
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
---
+++ helper.php
@@ -0,0 +1,48 @@
+<?php
+require 'Filter.php';
+//Tecnica postback
+//se secondo accesso prendo i dati dal form
+
+$filter = new Filter();
+
+$ftp_server = $filter->sanitize($_POST['ftp_server']);
+echo $ftp_server;
+$port = $filter->sanitize($_POST['port']);
+echo $port;
+$username = $filter->sanitize($_POST['username']);
+echo $username;
+$password = $filter->sanitize($_POST['password']);
+echo $password;
+
+//validazione dei parametri di connessione
+if (($ftp_server != 'ftp_server') && ($ftp_server != '')) {
+    if (($username != 'username') && ($username != '')) {
+        if (($password != 'password') && ($password != '')) {
+            //validazione nome dei file
+            if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+                $file = $_FILES['file']['tmp_name']; //nome file con percorso assoluto
+                $new_file = $_FILES['file']['name']; //nome file senza percorso
+
+                //apertura connessione ftp
+                $conn = ftp_connect($ftp_server, $port) or die ('Impossibile connettersi al server');
+                ftp_login($conn, $username, $password) or die ('username o password errati');
+                ftp_pasv($conn, true);
+
+                //upload del file
+                $invia = ftp_put($conn, $new_file, $file, FTP_BINARY);
+                echo (!$invia) ? 'Upload fallito' : 'Upload completato';
+
+                //chiusura connessione
+                ftp_close($conn);
+            } else {
+                echo "<b>Inserire il file</b>";
+            }
+        } else {
+            echo "<b>Inserire la password</b>";
+        }
+    } else {
+        echo "<b>Inserire lo username</b>";
+    }
+} else {
+    echo "<b>Inserire il server ftp</b>";
+}
